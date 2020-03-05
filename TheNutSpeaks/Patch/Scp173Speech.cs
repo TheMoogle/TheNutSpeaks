ecb3884137c8496a8384676dce9c5347bd5eb3d6
TheMoogle <34310114+TheMoogle@users.noreply.github.com>
2020-03-04 21:20:42 -0600

    Initial commit

---
+++ TheNutSpeaks/Patch/Scp173Speech.cs
@@ -0,0 +1,19 @@
+﻿using Assets._Scripts.Dissonance;
+using Harmony;
+
+namespace TheNutSpeaks.Patch
+{
+    [HarmonyPatch(typeof(DissonanceUserSetup), nameof(DissonanceUserSetup.CallCmdAltIsActive))]
+    public class Scp173Speech
+    {
+        public static bool Speech(DissonanceUserSetup __player, bool ef)
+        {
+            if (!Plugin.Enabled) return true;
+
+            CharacterClassManager ccm = __player.gameObject.GetComponent<CharacterClassManager>();
+            if (ccm.CurClass == RoleType.Scp173 || ccm.CurClass.Is939()) __player.MimicAs939 = ef;
+
+            return true;
+        }
+    }
+}
---
+++ TheNutSpeaks/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using Harmony;
+
+namespace TheNutSpeaks
+{
+    public class Plugin : EXILED.Plugin
+    {
+        public static bool Enabled;
+        public static int PatchCounter;
+
+        public override void OnEnable()
+        {
+            LoadConfigs();
+
+            if (!Enabled) return;
+
+            HarmonyInstance instance = HarmonyInstance.Create($"ur.mom.gay.thenutspeaks.{PatchCounter}");
+            instance.PatchAll();
+        }
+
+        public override void OnDisable()
+        {
+        }
+
+        private void LoadConfigs()
+        {
+            Enabled = Config.GetBool("173t_enabled", true);
+        }
+
+        public override void OnReload()
+        {
+
+        }
+
+        public override string getName { get; } = "NutSpeaks";
+    }
+}
